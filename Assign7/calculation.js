edd105f8d48903fbae13befff5fde370d3cab745
preeti <preetimadgul988@gmail.com>
2020-09-03 22:07:04 +0530

    initial commit

---
+++ Assign7/calculation.js
@@ -0,0 +1,24 @@
+function addNumbers()
+{
+	var Num1 = parseInt(document.getElementById('Number1').value);
+	var Num2 = parseInt(document.getElementById('Number2').value);
+	Result.value = Num1 + Num2;
+}
+function subNumbers()
+{
+	var Num1 = parseInt(document.getElementById('Number1').value);
+	var Num2 = parseInt(document.getElementById('Number2').value);
+	Result.value = Num1 - Num2;
+}
+function multiplyNumbers()
+{
+	var Num1 = parseInt(document.getElementById('Number1').value);
+	var Num2 = parseInt(document.getElementById('Number2').value);
+	Result.value = Num1 * Num2;
+}
+function divideNumbers()
+{
+	var Num1 = parseInt(document.getElementById('Number1').value);
+	var Num2 = parseInt(document.getElementById('Number2').value);
+	Result.value = Num1 / Num2;
+}
